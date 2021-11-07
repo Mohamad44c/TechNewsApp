ef89b47f91c33e040fc7f2b91290e47c1c234644
Mohamad Chebli <chebli17c@gmail.com>
2021-11-07 16:27:01 +0200

    Added Webview and render page

--- app/src/main/java/com/lb/technews/Articles.java
+++ app/src/main/java/com/lb/technews/Articles.java
@@ -5,8 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -19,38 +24,35 @@ import java.util.ArrayList;
 
 public class Articles extends AppCompatActivity {
     ArrayList<String> keyList;
-    ArrayList<String> article_titles;
-    int article_id;
-    String article_title;
-    String article_url;
+    ArrayList<String> article_titles = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_articles);
 
-        ListView myView = (ListView) findViewById(R.id.article_List);
 
         Bundle bundle = getIntent().getExtras();
         keyList = (ArrayList<String>) bundle.getStringArrayList("keysActual");
+
 //        retrieved parsed keys from main and printing 20
-        for (int i = 0; i < keyList.size(); i++) {
-            System.out.println("FROM MAIN: KEY # " + i + " " + keyList.get(i));
-        }
+//        for (int i = 0; i < keyList.size(); i++) {
+//            System.out.println("FROM MAIN: KEY # " + i + " " + keyList.get(i));
+//        }
+
 
         String articleURL = "https://hacker-news.firebaseio.com/v0/item/" + keyList.get(1) + ".json?print=pretty";
+        String articleURL2 = "https://hacker-news.firebaseio.com/v0/item/" + keyList.get(2) + ".json?print=pretty";
         DownloadTask task = new DownloadTask();
         task.execute(articleURL);
+        DownloadTask task2 = new DownloadTask();
+        task2.execute(articleURL2);
 
-        article_titles = new ArrayList<String>();
-        article_titles.add(article_title);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, article_titles);
-        myView.setAdapter(adapter);
 
 //        creating and filling database
 //        SQLiteDatabase db = this.openOrCreateDatabase("technewsdb", MODE_PRIVATE, null);
-//        db.execSQL("CREATE TABLE IF NOT EXISTS links (article_id INT(8), article_title VARCHAR, article_content VARCHAR)");
-//        db.execSQL("INSERT INTO links(article_id, article_title, article_content) VALUES()");
+//        db.execSQL("CREATE TABLE IF NOT EXISTS links (article_id INT(8), article_title VARCHAR, article_url VARCHAR)");
+//        db.execSQL("INSERT INTO links(article_id, article_title, article_url) VALUES()");
     }
 
     public class DownloadTask extends AsyncTask<String, Void, String> {
@@ -85,16 +87,35 @@ public class Articles extends AppCompatActivity {
         protected void onPostExecute(String result) {
             super.onPostExecute(result);
             try {
+                ListView myView = (ListView) findViewById(R.id.article_List);
+
+
                 JSONObject json = new JSONObject(result);
-                article_id = json.getInt("id");
-                article_title = json.getString("title");
-                article_url = json.getString("url");
+
+                int article_id = json.getInt("id");
+                String article_title = json.getString("title");
+                String article_url = json.getString("url");
+
 
                 Log.i("ID ", "" + article_id);
                 Log.i("Title ", article_title);
                 Log.i("URL ", article_url);
 
+                article_titles.add(article_title);
+                System.out.println("Article Size: " + article_titles.size());
+                System.out.println("Article URL: " + article_url);
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, article_titles);
+                myView.setAdapter(adapter);
 
+                myView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                        WebView webView = (WebView) findViewById(R.id.webView);
+                        webView.getSettings().setJavaScriptEnabled(true);
+                        webView.setWebViewClient(new WebViewClient());
+                        webView.loadUrl("https://en.wikipedia.org/wiki/Wikipedia:Wikipedia_Signpost/2021-10-31/Opinion");
+                    }
+                });
             } catch (Exception e) {
                 e.printStackTrace();
             }
